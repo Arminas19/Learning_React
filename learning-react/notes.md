3d2eaaabdc34be1e00d5ba5da88f9a21be862a0c
Arminas19 <armeska14@gmail.com>
2022-12-15 13:18:28 +0000

    created my first component

---
+++ learning-react/notes.md
@@ -0,0 +1,3 @@
+1. video - https://www.youtube.com/watch?v=f55qeKGgB_M
+
+time: 38:06
--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2> Company Job </h2>
+      <hr></hr>
+      <Job salary={90000} position="Senior Dev" company="apple" />
+      <Job salary={12000} position="mid-level Dev" company="microsoft" />
+      <Job salary={10000} position="Junior Dev" company="starbucks" />
     </div>
   );
+}
+
+const Job = (props) => {
+  return (
+    <div>
+      <h3> {props.position} - {props.salary} - {props.company} </h3>
+      <hr style={{ maxWidth: '300px' }} />
+    </div>
+  )
 }
 
 export default App;
