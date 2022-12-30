6b46b004dc6eb1dbc39c1c9edc988b56728b190f
Arminas19 <armeska14@gmail.com>
2022-12-30 11:36:46 +0000

    Created an application with prop-types

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Person } from './Person';
 
 export default function App() {
   return (
     <div className='App'>
-        <Person name={'Arminas'} email={'Test@test.com'} age={21} isMarried={true} friends={['Tom', 'Bob', 'Cillian', 'Mike', 'Timmy']}/>
+    App
     </div>
   );
 };
