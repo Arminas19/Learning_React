1d4dd120cef46c9332a2c0f7c9cc820726a1f50f
Arminas19 <armeska14@gmail.com>
2022-12-31 15:00:59 +0000

    getting set-up for fire-base project

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,34 +1,10 @@
-import {Provider} from 'react-redux';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
-import SignIn from './SignIn';
-import Contact from './Contact';
-import Home from './Home';
+import React from 'react';
 import './App.css';
-import { store } from './store';
 
 export default function App() {
   return (
-    <div className='App'> 
-    <Provider store={store}>
-    <BrowserRouter>
-        <div>
-          <nav>
-            <h1> Navbar</h1>
-            <Link to='/'> Home </Link>
-            <Link to='/signin'> Sign In </Link>
-            <Link to='/contact'> Contact </Link>
-            <br/>
-            <br/>
-            <br/>
-          </nav>
-        </div>
-          <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='signin' element={<SignIn />}/>
-          </Routes>
-        </BrowserRouter>
-        </Provider>
-        </div>
-  );
-};
+    <div className='App'>
+        App
+    </div>
+  )
+}
